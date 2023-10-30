eade524643b886fe31cf77f9339333ce50c031af
robert altmiller <robert.altmiller@databricks.com>
2023-10-30 09:52:04 -0500

    changes

--- shell_scripts/nonemptymerge.sh
+++ shell_scripts/nonemptymerge.sh
@@ -1,16 +1,35 @@
 #!/bin/bash
 
-base=$1
-current=$2
-other=$3
-output=$4
+# Program Execution for Resolving Merge Conflicts
+# Favor the main branch (REMOTE) content when both branches have differing non-empty content.
+# Choose the non-empty content if one branch's content is empty and the other's is not.
+# Default to the feature branch (LOCAL) content if not covered by the above scenarios.
 
-# Favor non-empty content over empty content
-if [[ -s $current ]] && [[ ! -s $other ]]; then
-    cat $current > $output
-elif [[ ! -s $current ]] && [[ -s $other ]]; then
-    cat $other > $output
+
+# Get the parameters passed by git
+BASE=$1
+LOCAL=$2
+REMOTE=$3
+MERGED=$4
+
+# If LOCAL is non-empty and differs from REMOTE
+if [[ -s $LOCAL ]] && ! cmp -s $LOCAL $REMOTE; then
+    # If REMOTE is also non-empty, favor main (REMOTE)
+    if [[ -s $REMOTE ]]; then
+        cp $REMOTE $MERGED
+    else
+        cp $LOCAL $MERGED
+    fi
+
+# If LOCAL is empty and REMOTE is non-empty
+elif [[ ! -s $LOCAL ]] && [[ -s $REMOTE ]]; then
+    cp $REMOTE $MERGED
+# If LOCAL is non-empty and REMOTE is empty
+elif [[ -s $LOCAL ]] && [[ ! -s $REMOTE ]]; then
+    cp $LOCAL $MERGED
 else
-    # Default to standard merge conflict if neither or both sides are non-empty
-    git merge-file $current $base $other
+    # Default case: just choose LOCAL (this shouldn't happen if you've covered all scenarios)
+    cp $LOCAL $MERGED
 fi
+
+exit 0
